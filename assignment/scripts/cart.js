95e41aabcdb2eaac0ee0593c63c4ad8c75f0248f
Zach Garrison <zgedina@yahoo.com>
2023-05-24 22:42:54 -0500

    Added basket and item variables, as well as a couple functions
    Added functions to list items, empty basket, add item

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,54 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+// ## Required Features
+// Update the `cart.js` file to do the following:
+
+// - Create a global variable named `basket` and set it to an empty array.
+
+// - Create a function called `addItem`. It should:
+//   - take an input parameter for a string `item`
+//   - add the new item to the global array `basket`. 
+//   - return `true` indicating the item was added
+
+// - Create a function called `listItems`. It should:
+//   - loop over the items in the `basket` array
+//   - console.log each individual item on a new line
+
+// - Create a function called `empty`. It should:
+//   - reset the `basket` to an empty array
+
+// > __IMPORTANT__
+// > Make sure that you are writing code *in the file* to test every function that you write!
+
+// For example, to test `addItem`:
+// ```
+// console.log(`Basket is ${basket}`);
+// console.log('Adding apples (expect true)', addItem('apples'));
+// console.log(`Basket is now ${basket}`);
+// ```
+
+let basket = []
+let item = ''
+function addItem(item) {
+basket.push(item)
+if (basket.includes(item)){
+    return true
+} else return false
+}
+console.log(addItem("Banana"))
+
+function listItems() {
+    console.log(basket.map(item => item))
+}
+
+
+function emptyBasket(){
+  basket = []
+}
+addItem("Apple")
+addItem("Strawberry")
+listItems()
 
 
 
